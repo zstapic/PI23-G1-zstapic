88680beaeda5f4ad1342e57e04e6301353e4563c
zstapic <zstapic@foi.hr>
2023-05-22 13:17:17 +0200

    Završetak vježbe 13

---
+++ DBLayer/DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBLayer
+{
+    public static class DB
+    {
+        private static string _connectionString = @"Data Source=31.147.204.119\PISERVER,1433; Initial Catalog=EvaluationManager; User=korisnik; Password=lozinka";
+        private static SqlConnection _connection;
+        
+        public static void OpenConnection()
+        {
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+        
+        public static void CloseConnection()
+        {
+            if (_connection.State != System.Data.ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+
+        public static SqlDataReader GetDataReader(string query)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            return command.ExecuteReader();
+        }
+        
+        public static int ExecuteCommand(string sqlCommand)
+        {
+            SqlCommand command = new SqlCommand(sqlCommand, _connection);
+            return command.ExecuteNonQuery();
+        }
+    }
+}
